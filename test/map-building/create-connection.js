9dfa9db134cffdcd6df8482ac8fee03d6cdb71f7
Jonathan Birkholz <rookieone@gmail.com>
2015-01-12 13:29:09 -0600

    simple create connection

---
+++ test/map-building/create-connection.js
@@ -0,0 +1,15 @@
+var should = require("should");
+var mapBuilder = require("../../lib/map-building.js");
+
+describe("createConnection", function() {
+  it("should create connection", function() {
+    var map = mapBuilder.newMap();
+    mapBuilder.createConnection(map, "taxi", 10, 20);
+
+    should(map.nodes[10]).not.equal(undefined);
+    should(map.nodes[20]).not.equal(undefined);
+
+    var connection = map.connections[0];
+    should(map.connections.length).equal(1);
+  });
+});
