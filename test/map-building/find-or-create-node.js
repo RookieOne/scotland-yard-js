1f64b6463a8ddf855ad95fe678c67314f5c52f9a
Jonathan Birkholz <rookieone@gmail.com>
2015-01-12 13:24:29 -0600

    updated find or create node test

--- test/map-building/find-or-create-node.js
+++ test/map-building/find-or-create-node.js
@@ -3,17 +3,17 @@ var mapBuilder = require("../../lib/map-building.js");
 
 describe("findOrCreateNode", function() {
   it("should return new node if node does not exist", function() {
-    var nodes = [];
-    mapBuilder.findOrCreateNode(nodes, 10);
-    should(nodes[10]).not.equal(undefined);
-    should(nodes[33]).equal(undefined);
+    var map = mapBuilder.newMap();
+    mapBuilder.findOrCreateNode(map, 10);
+    should(map.nodes[10]).not.equal(undefined);
+    should(map.nodes[33]).equal(undefined);
   });
 
   it("should return node if node does exist", function() {
-    var nodes = [];
-    mapBuilder.findOrCreateNode(nodes, 10);
-    mapBuilder.findOrCreateNode(nodes, 10);
-    should(nodes[10]).not.equal(undefined);
-    should(nodes[33]).equal(undefined);
+    var map = mapBuilder.newMap();
+    mapBuilder.findOrCreateNode(map, 10);
+    mapBuilder.findOrCreateNode(map, 10);
+    should(map.nodes[10]).not.equal(undefined);
+    should(map.nodes[33]).equal(undefined);
   });
 });
